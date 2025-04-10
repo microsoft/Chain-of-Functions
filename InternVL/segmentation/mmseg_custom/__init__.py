a8215481d23998df67fd21e32f6cca9b9ca688a1
zli999 <zijian.li@connect.ust.hk>
2025-04-10 06:14:28 +0000

    remove internvl

--- InternVL/segmentation/mmseg_custom/__init__.py
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------
-# InternVL
-# Copyright (c) 2023 OpenGVLab
-# Licensed under The MIT License [see LICENSE for details]
-# --------------------------------------------------------
-from .datasets import *  # : F401,F403
-from .models import *  # : F401,F403
--- InternVL/segmentation/mmseg_custom/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# --------------------------------------------------------
-# InternVL
-# Copyright (c) 2023 OpenGVLab
-# Licensed under The MIT License [see LICENSE for details]
-# --------------------------------------------------------
-from .backbones import *  # : F401,F403
-from .decode_heads import *  # : F401,F403
